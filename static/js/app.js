50faac31bba07ffadfec41f6d660e56e01822674
Bill Johnson <wpj174@gmail.com>
2022-01-07 20:47:02 -0500

    Update app.js
    Bar chart works

--- static/js/app.js
+++ static/js/app.js
@@ -19,7 +19,7 @@ function demoInfo(sample)
 
         // get item 0 from the array
         let resultData = result[0];
-        console.log(resultData);
+        //console.log(resultData);
 
         // clear the metadata if any for next round of populating
         d3.select("#sample-metadata").html("");
@@ -34,6 +34,60 @@ function demoInfo(sample)
 }
 
 // function to build the graphs
+function buildBarChart(sample)
+{
+    //console.log(sample);
+
+    //let data = d3.json("samples.json");
+    //console.log(data);
+
+    d3.json("samples.json").then((data) => {
+        // get the sample data
+        let sampleData = data.samples;
+        console.log(sampleData);
+
+        // filter based on the sample number passed in - should get a single-item array
+        let result = sampleData.filter(sampleResult => sampleResult.id == sample);
+
+        //console.log(result);
+
+        // get item 0 from the array
+        let resultData = result[0];
+        //console.log(resultData);
+
+        // get otu_ids & otu_labels
+        let otu_ids = resultData.otu_ids;
+        let otu_labels = resultData.otu_labels;
+        let sample_values = resultData.sample_values;
+
+        //console.log(otu_ids);
+        //console.log(otu_labels);
+        //console.log(sample_values);
+
+        // buld the bar chart
+        // get the y ticks
+        let yticks = otu_ids.slice(0, 10).map(id => `OTU ${id}`);
+        let xValues = sample_values.slice(0, 10);
+        let textLabels = otu_labels.slice(0, 10);
+        //console.log(textLabels);
+
+        let barChart = {
+            y: yticks.reverse(),
+            x: xValues.reverse(),
+            text: textLabels.reverse(),
+            type: "bar",
+            orientation: "h"
+        };
+
+        let layout = {
+            title: "Top 10 Belly Button Bactieria"
+        };
+
+        Plotly.newPlot("bar", [barChart], layout);
+
+    });
+
+}
 
 // function to initialize the dashboard
 function initialize()
@@ -63,6 +117,9 @@ function initialize()
         // call the function to build the metadata
         demoInfo(sample1);
 
+        // call function to build the barchart
+        buildBarChart(sample1);
+
     });
 }
 
@@ -71,6 +128,9 @@ function optionChanged(item)
 {
     // call the update to the metadata
     demoInfo(item);
+
+    // call fuction to build the barchart
+    buildBarChart(item);
 
 }
 
